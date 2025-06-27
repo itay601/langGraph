750e1c97b14b757ed70c295a6cb4d9d0c312dc8c
itay601 <itaymerel1212@gmail.com>
2025-06-27 20:57:53 +0300

    advance chatbot setup using firecrawl

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-from purpose.chatbot.chatbot import get_chatbot_response
-from purpose.chatbot.models import ChatRequest, ChatResponse
+from chatbot.chatbot import get_chatbot_response
+from chatbot.models import ChatRequest, ChatResponse
+#from chatbotWithTools.+++ import
+#from chatbotWithTools.+++ import
 
 app = FastAPI()
-
-@app.post("/chat", response_model=ChatResponse)
-def chat(req: ChatRequest):
-    reply = get_chatbot_response(req.message)
-    #return {"response": reply}
-    return ChatResponse(response=reply)
 
 @app.get("/")
 def root():
@@ -17,3 +13,15 @@ def root():
 @app.get("/health")
 def root():
     return {"msg":"server is healthy"}     
+
+@app.post("/chatbot", response_model=ChatResponse)
+def chat(req: ChatRequest):
+    reply = get_chatbot_response(req.message)
+    #return {"response": reply}
+    return ChatResponse(response=reply)
+
+@app.post("/chatbottools", response_model=ChatResponse)
+def chat(req: ChatRequest):
+    reply = get_chatbot_response(req.message)
+    #return {"response": reply}
+    return ChatResponse(response=reply)
